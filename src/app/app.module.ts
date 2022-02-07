3c19c490a27f0d692040dc0cd34cddb5d25f5c6a
kenhyj <mathkenjeong@gmail.com>
2022-02-06 18:50:55 -0800

    ng generate component signin + looking into html request.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+// https://angular.io/tutorial/toh-pt6
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
